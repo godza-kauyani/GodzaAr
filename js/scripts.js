a3adf1311ad750b26c080cfc4d625c35b0eb86e0
kauyani-godfrey <kauyanigodfrey@gmail.com>
2018-12-27 11:20:12 +0200

    init

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+UpUp.start({
+    'content-url' :'index.html',
+	'assets':[
+		'https://aframe.io/releases/0.6.1/aframe.min.js',
+		'https://jeromeetienne.github.io/AR.js/aframe/build/aframe-ar.js',
+		'/js/scripts.js'
+	]});
